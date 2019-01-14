54731301af3d0bea3591041ee59d493d287bbc4e
David Núñez <david@nucypher.com>
2019-01-14 11:03:01 +0100

    First pass on integrating car_session code in the project

--- car/car_session.py
+++ car/car_session.py
@@ -1,8 +1,6 @@
-import random
 import time
 import msgpack
 import sqlite3
-import json
 import paho.mqtt.client as mqtt
 
 from nucypher.data_sources import DataSource
@@ -12,6 +10,8 @@ from umbral.keys import UmbralPublicKey
 pub_key_bytes = b'\x03\x07a\xebt|&\x8d\xb6\xb7\xd5b\xf1\x8f\xe1,\xf9n1\xa7\xcf\xe0\xec\xff~E\xdd\x8c.\x8bB\xe4\xbd'
 
 DATA_FILENAME = 'car_data.msgpack'
+RECORDED_CAR_SESSION = "UMA-5_10_17-Simulation.db"
+
 MQTT_USERNAME = '10288942'
 MQTT_PASSWD = '184def19b4bbb41a'
 MQTT_HOST = "broker.shiftr.io"
@@ -22,114 +22,116 @@ MQTT_PORT = 1883
 DEFAULT_LABEL = b"Alicia's car data"
 
 
-def generate_car_simulation_samples ( policy_pubkey, label: bytes = DEFAULT_LABEL, save_as_file: bool = False, send_by_mqtt: bool = False ):
+def generate_car_simulation_samples(policy_pubkey,
+                                    label: bytes = DEFAULT_LABEL,
+                                    save_as_file: bool = False,
+                                    send_by_mqtt: bool = False):
+    data_source = DataSource(policy_pubkey_enc=policy_pubkey, label=label)
 
-	data_source = DataSource(policy_pubkey_enc=policy_pubkey, label=label)
+    data_source_public_key = bytes(data_source.stamp)
 
-	data_source_public_key = bytes(data_source.stamp)
+    # path of session database file
+    sessionPath = RECORDED_CAR_SESSION
 
-	# path of session database file
-	sessionPath = "UMA-5_10_17-Simulation.db"
+    if send_by_mqtt:
+        # Connect to MQTT platform
+        client = mqtt.Client()
+        client.username_pw_set(MQTT_USERNAME, MQTT_PASSWD)
+        client.connect(MQTT_HOST, MQTT_PORT, 60)
 
-	if send_by_mqtt:
+        # Communication is starting: send public key
+        client.publish("/Alicia_Car_Data/public_key", pub_key_bytes)
+        client.publish("/Alicia_Car_Data/data_source_public_key", data_source_public_key)
 
-		# Connect to MQTT platform
-		client = mqtt.Client()
-		client.username_pw_set(MQTT_USERNAME, MQTT_PASSWD)
-		client.connect(MQTT_HOST, MQTT_PORT, 60)
 
-		# Communication is starting: send public key
-		client.publish("/Alicia_Car_Data/public_key", pub_key_bytes)
-		client.publish("/Alicia_Car_Data/data_source_public_key", data_source_public_key)
+    # Message kits list
+    kits = list()
+    try:
+        # Connection to saved session database
+        db = sqlite3.connect(sessionPath)
+        tripCurs = db.cursor()
+        gpsCurs = db.cursor()
+        obdCurs = db.cursor()
+        beacons_dataCurs = db.cursor()
 
-	try:
-		# Connection to saved session database
-		db=sqlite3.connect(sessionPath)
-		tripCurs = db.cursor()
-		gpsCurs = db.cursor()
-		obdCurs = db.cursor()
-		beacons_dataCurs = db.cursor()
+        # Data Base cursor for beacons data table
+        beacons_dataCurs.execute("SELECT * FROM beacons_data")
+        # take the first beacon data row from table
+        beacons_dataRow = beacons_dataCurs.fetchone()
 
-		# Message kits list
-		kits = list()
+        # everytime that engine stop and start during session saving, new trip is created
+        for trip in tripCurs.execute("SELECT * FROM trip"):
+            start = trip[1]
+            end = trip[2]
 
-		# Data Base cursor for beacons data table
-		beacons_dataCurs.execute("SELECT * FROM beacons_data")
-		# take the first beacon data row from table
-		beacons_dataRow = beacons_dataCurs.fetchone()
+            nextTime = None
 
-		# everytime that engine stop and start during session saving, new trip is created
-		for trip in tripCurs.execute("SELECT * FROM trip"):
-			start = trip[1]
-			end = trip[2]
+            for gpsRow in gpsCurs.execute("SELECT * FROM gps WHERE time>=(?) AND time<(?)", (start, end)):
+                # if this is not the first iteration...
+                if nextTime != None:
+                    currentTime = nextTime
+                    nextTime = gpsRow[6]
 
-			nextTime = None
+                    # time difference between two samples
+                    diff = nextTime - currentTime
 
-			for gpsRow in gpsCurs.execute("SELECT * FROM gps WHERE time>=(?) AND time<(?)",(start,end)):
-				# if this is not the first iteration...
-				if nextTime != None:
-					currentTime = nextTime
-					nextTime = gpsRow[6]
+                    # sleep the thread: simulating gps signal delay
+                    time.sleep(0.01)
 
-					# time difference between two samples
-					diff = nextTime - currentTime
+                    # take the same sample from obd table
+                    obdCurs.execute("SELECT * FROM obd WHERE time=(?)", (currentTime,))
+                    obdRow = obdCurs.fetchone()
 
-					# sleep the thread: simulating gps signal delay
-					time.sleep(0.01)
+                    # obtained information about OBDII & GPS from sessions database
+                    temp = int(obdRow[0])
+                    rpm = int(obdRow[1])
+                    vss = int(obdRow[2])
+                    maf = obdRow[3]
+                    throttlepos = obdRow[4]
+                    lat = gpsRow[0]
+                    lon = gpsRow[1]
+                    alt = gpsRow[2]
+                    gpsSpeed = gpsRow[3]
+                    course = int(gpsRow[4])
+                    gpsTime = int(gpsRow[5])
 
-					# take the same sample from obd table
-					obdCurs.execute("SELECT * FROM obd WHERE time=(?)",(currentTime,))
-					obdRow = obdCurs.fetchone()
+                    # structure for generating msgpack
+                    car_data = {"carInfo": {"engineOn": True, "temp": temp, "rpm": rpm, "vss": vss, "maf": maf,
+                                            "throttlepos": throttlepos, "lat": lat, "lon": lon, "alt": alt,
+                                            "gpsSpeed": gpsSpeed, "course": course, "gpsTime": gpsTime}}
 
-					# obtained information about OBDII & GPS from sessions database
-					temp = int(obdRow[0])
-					rpm = int(obdRow[1])
-					vss = int(obdRow[2])
-					maf = obdRow[3]
-					throttlepos = obdRow[4]
-					lat = gpsRow[0]
-					lon = gpsRow[1]
-					alt = gpsRow[2]
-					gpsSpeed = gpsRow[3]
-					course = int(gpsRow[4])
-					gpsTime = int(gpsRow[5])
-					
-					# structure for generating msgpack
-					car_data = {"carInfo": {"engineOn":True, "temp":temp, "rpm":rpm, "vss":vss, "maf":maf, "throttlepos":throttlepos, "lat":lat, "lon":lon, "alt":alt, "gpsSpeed":gpsSpeed, "course":course, "gpsTime":gpsTime} }				
+                    plaintext = msgpack.dumps(car_data, use_bin_type=True)
+                    message_kit, _signature = data_source.encrypt_message(plaintext)
 
-					plaintext = msgpack.dumps(car_data, use_bin_type=True)
-					message_kit, _signature = data_source.encrypt_message(plaintext)
+                    kit_bytes = message_kit.to_bytes()
+                    kits.append(kit_bytes)
 
-					kit_bytes = message_kit.to_bytes()
-					kits.append(kit_bytes)
+                    if send_by_mqtt:
+                        client.publish("/Alicia_Car_Data", kit_bytes)
 
-					if send_by_mqtt:
-						client.publish("/Alicia_Car_Data", kit_bytes)
+                else:
+                    nextTime = gpsRow[6]
 
-				else:
-					nextTime = gpsRow[6]
+    finally:
+        if db:
+            db.close()
 
-	finally:
-		if (db != None):
-			db.close()
+        client.publish("/Alicia_Car_Data/end", "end")
 
-		client.publish("/Alicia_Car_Data/end", "end")
+        data = {
+            'data_source': data_source_public_key,
+            'kits': kits,
+        }
 
-		data = {
-			'data_source': data_source_public_key,
-			'kits': kits,
-		}
+        if save_as_file:
+            with open(DATA_FILENAME, "wb") as file:
+                msgpack.dump(data, file, use_bin_type=True)
 
-		if save_as_file:
-			with open(DATA_FILENAME, "wb") as file:
-				msgpack.dump(data, file, use_bin_type=True)
+        return data
 
-		return data
 
-	
-# Only for developing and testing purpouses. Comment if you call the functions from other script
+# Only for developing and testing purposes.
 if __name__ == "__main__":
-	
-	pub_key = UmbralPublicKey.from_bytes(pub_key_bytes)
+    pub_key = UmbralPublicKey.from_bytes(pub_key_bytes)
 
-	generate_car_simulation_samples(pub_key, save_as_file = True, send_by_mqtt = True)
+    generate_car_simulation_samples(pub_key, save_as_file=True, send_by_mqtt=True)
